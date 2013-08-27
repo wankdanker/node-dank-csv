089155a013785ad04d6fee8680d02e50934ad22c
Dan VerWeire <dverweire@gmail.com>
2013-08-27 16:47:16 -0400

    added failing test; will probably abort this module now because better ones exist

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var parse = require('../');
 
 testData1 = 'col1, col2, col3\n1234, 1234, 1234\n1235, asdf, fds23';
 testData2 = 'col1	col2	col3\n1234	1234	1234\n1235	asdf	fds23';
+testData3 = 'col1, col2, col3\n"something, something, dark side", 1234, 1234\n"hello,world", 1345, 245e';
 
 exports['test comma separated'] = function (test) {
 	a = parse(testData1);
@@ -26,5 +27,18 @@ exports['test tab separated'] = function (test) {
 		]
 	)
 	
+	test.done();
+}
+
+exports['test with text string containing double quotes'] = function (test) {
+	a = parse(testData3);
+
+	test.deepEqual(a,
+		[
+			{ col1 : 'something, something, dark side', col2 : '1234', col3 : '1234' },
+			{ col1 : 'hello,world', col2 : '1345', col3 : '245e' }
+		]
+	);
+
 	test.done();
 }
